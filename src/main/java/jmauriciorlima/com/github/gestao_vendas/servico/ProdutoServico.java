ad2e7c7c70842a89fb53646df053ca5807c58a01
José Maurício <mauricio.rodriguesdelima@gmail.com>
2024-08-20 19:15:21 -0300

    Criando validação de produto já cadastrado e lançando exceção ocm Exception Handler.

--- src/main/java/jmauriciorlima/com/github/gestao_vendas/servico/ProdutoServico.java
+++ src/main/java/jmauriciorlima/com/github/gestao_vendas/servico/ProdutoServico.java
@@ -1,7 +1,10 @@
 package jmauriciorlima.com.github.gestao_vendas.servico;
 
+import jmauriciorlima.com.github.gestao_vendas.entidades.Categoria;
 import jmauriciorlima.com.github.gestao_vendas.entidades.Produto;
+import jmauriciorlima.com.github.gestao_vendas.excecao.RegraNegocioException;
 import jmauriciorlima.com.github.gestao_vendas.repositorio.ProdutoRepostirotio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,11 +13,11 @@ import java.util.Optional;
 @Service
 public class ProdutoServico {
 
-    private ProdutoRepostirotio produtoRepostirotio;
+    @Autowired
+    private CategoriaServico categoriaServico;
 
-    public ProdutoServico(ProdutoRepostirotio produtoRepostirotio) {
-        this.produtoRepostirotio = produtoRepostirotio;
-    }
+    @Autowired
+    private ProdutoRepostirotio produtoRepostirotio;
 
     public List<Produto> listarTodosPorCategoria(Long condigoCategoria) {
         return produtoRepostirotio.findByCategoriaCodigo(condigoCategoria);
@@ -25,6 +28,27 @@ public class ProdutoServico {
     }
 
     public Produto salvar(Produto produto) {
+        validarCategoriaDoProdutoExiste(produto.getCategoria().getCodigo());
+        validarProdutoDuplicado(produto);
         return produtoRepostirotio.save(produto);
+    }
+
+    private void validarProdutoDuplicado(Produto produto) {
+        if (produtoRepostirotio.findByCategoriaCodigoAndDescricao(
+                produto.getCategoria().getCodigo(),
+                produto.getDescricao()).isPresent()) {
+            throw new RegraNegocioException(String.format("O produto %s já está cadastrado.", produto.getDescricao()));
+        }
+    }
+
+    private void validarCategoriaDoProdutoExiste(Long codigoCategoria) {
+        if (codigoCategoria == null) {
+            throw new RegraNegocioException("A categoria não pode ser nula");
+        }
+        if (categoriaServico.buscarPorCodigo(codigoCategoria).isEmpty()) {
+            throw  new RegraNegocioException(
+                    String.format("A categoria de código %s informada não existe no cadastro", codigoCategoria));
+        }
+
     }
 }
